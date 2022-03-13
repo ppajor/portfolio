f735d2e8ebab7e8f8c3a499354116087fa2335bd
ppajor <pawelpajor8@wp.pl>
2022-03-13 16:07:30 +0100

    Initial commit - building hero section

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,26 @@
+import React from "react"
+import * as styles from "./hero.module.css"
+import ParallaxSVG from "../ParallaxSVG"
+
+function Hero({ headerRef, paragraphRef, svgRef }) {
+  return (
+    <div className={styles.hero}>
+      <section className={styles.leftSection}>
+        <h1 className={styles.heroHeader} ref={headerRef}>
+          Frontend Developer
+        </h1>
+        <p className={styles.heroParagraph} ref={paragraphRef}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Hac enim
+          donec vitae tortor. At sem enim integer mi accumsan, morbi massa
+          tincidunt non. Diam blandit dignissim sem enim arcu, tellus, fringilla
+          lorem.
+        </p>
+      </section>
+      <div className={styles.rightSection} ref={svgRef}>
+        <ParallaxSVG />
+      </div>
+    </div>
+  )
+}
+
+export default Hero
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,61 @@
-import React from "react"
+import React, { useEffect, useRef } from "react"
+import { gsap } from "gsap"
+import * as styles from "../styles/index.module.css"
+import logoXD from "../assets/logoXD.png"
+import Nav from "../components/Nav/Nav"
+import Hero from "../components/Hero/Hero"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  const animation = useRef()
+  const navRef = useRef()
+  const headerRef = useRef()
+  const pRef = useRef()
+  const svgRef = useRef()
+
+  useEffect(() => {
+    var tl = gsap.timeline()
+
+    tl.to(animation.current, {
+      top: window.innerHeight,
+      duration: 1,
+      delay: 0.2,
+      ease: "power3.in",
+    })
+    tl.from(navRef.current, {
+      yPercent: -100,
+      duration: 0.8,
+      ease: "power2.in",
+    })
+    tl.from(headerRef.current, {
+      duration: 1,
+      xPercent: -120,
+      ease: "power2.in",
+    })
+    tl.from(pRef.current, {
+      duration: 0.5,
+      xPercent: -120,
+      ease: "power2.in",
+    })
+    tl.to(svgRef.current, {
+      opacity: 1,
+      duration: 2,
+      ease: "power2.in",
+    })
+  }, [])
+
+  return (
+    <>
+      <div
+        className={styles.animationContainer}
+        id="animationPage"
+        ref={animation}
+      >
+        <img className={styles.img} src={logoXD} alt="logo" />
+      </div>
+      <div className={styles.mainContainer}>
+        <Nav navRef={navRef} />
+        <Hero headerRef={headerRef} paragraphRef={pRef} svgRef={svgRef} />
+      </div>
+    </>
+  )
 }
